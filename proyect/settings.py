8936fa1ebe8fa529dc7aae84a031affba0c30b62
Don Señor <sandovalhmarcos@gmail.com>
2025-03-12 18:08:34 -0300

    agregando allowed_hosts para ver si ese es el error

--- proyect/settings.py
+++ proyect/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['cursodjango.parentesisboutique.cl', '127.0.0.1']
+# ALLOWED_HOSTS = ['cursodjango.parentesisboutique.cl', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
